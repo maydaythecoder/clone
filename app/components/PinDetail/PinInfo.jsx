3f94a6a042b6ce52aab66678566b681eec568cf3
mayday <maydaythecoder@gmail.com>
2024-05-14 21:05:31 -0400

    styling changes to PinInfo

--- app/components/PinDetail/PinInfo.jsx
+++ app/components/PinDetail/PinInfo.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 import UserTag from "../UserTag";
 
 function PinInfo({ pinDetail }) {
@@ -8,10 +15,10 @@ function PinInfo({ pinDetail }) {
     image: pinDetail.userImage,
   };
   return (
-    <div>
+    <div className=" align-middle">
       <button
         className="p-2 text-[23px]
-      mt-10 rounded-full  transition-all"
+      mt-10 absolute rounded-full  transition-all"
         onClick={() => window.open(pinDetail.link)}
       >
         <svg
@@ -27,12 +34,11 @@ function PinInfo({ pinDetail }) {
         </svg>
       </button>
 
-      <button
-        className="p-2 text-[23px]
-      mt-10 rounded-full  transition-all"
-        onClick={() => window.open(pinDetail.link)}
-      >
-        <svg
+      <Menu as="div" className=" inline-block text-left absolute mt-1">
+      <div>
+        <Menu.Button className="p-2 text-[23px]
+      mt-10 absolute ml-10 rounded-full  transition-all">
+          <svg
           aria-hidden="true"
           aria-label=""
           className=""
@@ -43,11 +49,175 @@ function PinInfo({ pinDetail }) {
         >
           <path d="M12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6M3 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6m18 0a3 3 0 1 0 0 6 3 3 0 0 0 0-6"></path>
         </svg>
-      </button>
 
-      <h2 className="text-[30px] font-bold mb-10">{pinDetail.title}</h2>
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-50 top-20 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Account settings
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Support
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  License
+                </a>
+              )}
+            </Menu.Item>
+            <form method="POST" action="#">
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    type="submit"
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block w-full px-4 py-2 text-left text-sm'
+                    )}
+                  >
+                    Sign out
+                  </button>
+                )}
+              </Menu.Item>
+            </form>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+
+      <Menu as="div" className=" inline-block text-left absolute mt-11 ml-72 ">
+      <div>
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 mb-2 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900">
+          Board
+          <ChevronDownIcon className="-mr-1 h-5 w-5 text-black font-bold" aria-hidden="true" />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Account settings
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Support
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  License
+                </a>
+              )}
+            </Menu.Item>
+            <form method="POST" action="#">
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    type="submit"
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block w-full px-4 py-2 text-left text-sm'
+                    )}
+                  >
+                    Sign out
+                  </button>
+                )}
+              </Menu.Item>
+            </form>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+
+    <button
+        type="button"
+        className=" absolute mt-11 ml-96 rounded-full bg-red-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+      >
+        Save
+      </button>
+            <div className="mt-28 absolute">
+      <h2 className="text-[30px] font-bold mb-5 w-96">{pinDetail.title}</h2>
+      <h2 className="mb-10 w-96">{pinDetail.desc}</h2>
       <UserTag user={user} />
-      <h2 className="mt-10">{pinDetail.desc}</h2>
+      <button
+        type="button"
+        className=" absolute -mt-9 ml-80 rounded-full bg-gray-100 px-4 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+      >
+        Follow
+      </button>
+      </div>
     </div>
   );
 }
