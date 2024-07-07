6acba900af0e29844d318355394b7b7e5b0e63bf
mayday <maydaythecoder@gmail.com>
2024-07-07 16:59:22 -0400

    reformated

--- app/api/images/route.js
+++ app/api/images/route.js
@@ -1,24 +1,23 @@
 export const GET = async () => {
-    try {
-
-        const response = await fetch(process.env.IMAGE_API_URL);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const contentType = response.headers.get("content-type");
-        if (!contentType || !contentType.includes("application/json")) {
-            throw new Error('Expected JSON response');
-        }
-
-        const parsedData = await response.json();
-        if (!Array.isArray(parsedData)) {
-            throw new Error('Expected an array of data');
-        }
-
-        return parsedData; // Assuming parsedData is an array of image objects
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        return { error: `Failed to fetch data: ${error.message}` };
+  try {
+    const response = await fetch(process.env.IMAGE_API_URL);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+
+    const contentType = response.headers.get("content-type");
+    if (!contentType || !contentType.includes("application/json")) {
+      throw new Error("Expected JSON response");
+    }
+
+    const parsedData = await response.json();
+    if (!Array.isArray(parsedData)) {
+      throw new Error("Expected an array of data");
+    }
+
+    return parsedData;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return { error: `Failed to fetch data: ${error.message}` };
+  }
 };
--- app/components/test.js
+++ app/components/test.js
@@ -1,38 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import { GET } from '../api/images/route';
+import React, { useEffect, useState } from "react";
+import { GET } from "../api/images/route";
 import DropDown from "./DropDownMenu";
 import ShareButton from "./ShareButton";
 
 export default function Test() {
-    const [data, setData] = useState([]);
-    const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await GET();
-            if (result.error) {
-                setError(result.error);
-            } else {
-                setData(result);
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await GET();
+      if (result.error) {
+        setError(result.error);
+      } else {
+        setData(result);
+      }
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
-    if (data.length === 0) {
-        return <div>Loading...</div>;
-    }
+  if (data.length === 0) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <>
-        <div>
-            {data.map((item) => (
-                <div key={item.id} className="relative 
+  return (
+    <>
+      <div>
+        {data.map((item) => (
+          <div
+            key={item.id}
+            className="relative 
        before:absolute
        before:h-full before:w-full
        before:rounded-3xl
@@ -41,20 +43,22 @@ export default function Test() {
        before:opacity-50
        cursor-pointer
        my-5
-       ">
-                    {/* <img src={item.urls.regular} alt={item.alt_description} /> */}
-                    {/* <h1>{item.alt_description}</h1> */}
-                    <img src={item.urls.small} alt={item.alt_description}
-                    className="rounded-3xl 
+       "
+          >
+            {/* <img src={item.urls.regular} alt={item.alt_description} /> */}
+            {/* <h1>{item.alt_description}</h1> */}
+            <img
+              src={item.urls.small}
+              alt={item.alt_description}
+              className="rounded-3xl 
         cursor-pointer relative z-0 "
-         />
-                    {/* <p>{item.description}</p> */}
-                    <DropDown />
-      <ShareButton />
-                </div>
-            ))}
-
-        </div>
-        </>
-    );
+            />
+            {/* <p>{item.description}</p> */}
+            <DropDown />
+            <ShareButton />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
