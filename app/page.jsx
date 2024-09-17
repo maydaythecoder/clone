ef798863014fd69d5e9cfb60f8bbefd7dee6a4ad
mayday <maydaythecoder@gmail.com>
2024-09-17 16:02:00 -0400

    updated comments

--- app/page.jsx
+++ app/page.jsx
@@ -7,14 +7,13 @@ import { Categories } from "./components/Categories";
 import Header from "./components/Header";
 import Test from "./components/test";
 
-//TODO: fix navbar to track user interaction and state
+//TODO: improve explore page and categories to work only with provided categories
 //TODO: add post interactions with proper links
-//TODO: improve explore page to work only with provided categories
 //TODO: improve user page to work with api data(get all posts by user)
-//TODO: improve comment accordian to add replies
-//TODO: fix pin/[pinId]/page.jsx add recomendations to show similar pins 
-//TODO: fix pin/[pinId]/page.jsx add animation to comment accordian
 //TODO: fix pin/[pinId]/page.jsx add media element to individual comments in the comment accordian
+//TODO: improve comment accordian to add replies
+//TODO: fix navbar to track user interaction and state
+//TODO: fix pin/[pinId]/page.jsx add recomendations to show similar pins 
 //TODO: add interaction to search bar
 //TODO: fix comment accordian to only show part of it, set overflow to scroll and add comment interactions
 //TODO: add error handling
